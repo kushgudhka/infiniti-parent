ce33685a237a58b223dcdf897d907b58bbc9c7c8
kushGudhka <kushgudhka22@gmail.com>
2025-03-29 14:52:54 +0530

    INFINITI: Added Config Filter

---
+++ src/main/java/in/aceinvesting/parent/config/WebSocketConfig.java
@@ -0,0 +1,19 @@
+package in.aceinvesting.parent.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @Author: Kush Gudhka
+ */
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").setAllowedOrigins("*");
+    }
+}
